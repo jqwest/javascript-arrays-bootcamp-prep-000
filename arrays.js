97cd16e2df2153667b2df233838228110d70ba30
john L <johnleavell@gmail.com>
2019-08-10 16:11:36 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -10,23 +10,20 @@ var candyString = "milky way"
 
 function addElementToBeginningOfArray(chocolateBars, candyString) {
   return [candyString, ...chocolateBars];
-
 }
 
 function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString) {
 chocolateBars.unshift(candyString);
 return chocolateBars;
-
-
 }
 
 function addElementToEndOfArray(chocolateBars, candyString) {
   return [...chocolateBars, candyString];
 }
 
-function destructivelyAddElementToEndOfArray(chocolateBars, candyString) {
-  chocolateBars.push(candyString);
-  retrun choclateBars;
+function destructivelyAddElementToEndOfArray(chocolateBars, candyString){
+chocolateBars.push(candyString);
+return chocolateBars;
 }
 
 
